20ce4dd8a39a1af4c47c0655c9a9dd0597a26507
Lulabs <lulabs@proton.me>
2026-05-05 13:42:17 +0200

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// IMPORTANT : remplace 'finances' par le nom exact de ton repo GitHub.
-// Si tu déploies à la racine d'un domaine custom, mets simplement '/'.
-export default defineConfig({
-  plugins: [react()],
-  base: '/finances/',
-});
